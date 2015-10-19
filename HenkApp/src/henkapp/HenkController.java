44da156fc96d2bbb14c793ea192b9797dabc150f
Styntje <styntje@hotmail.nl>
2015-10-19 18:09:25 +0200

    shit gefixt, jullie weten hoe ik rollllll

--- HenkApp/src/henkapp/HenkController.java
+++ HenkApp/src/henkapp/HenkController.java
@@ -13,14 +13,17 @@ import javafx.collections.FXCollections;
 import static javafx.collections.FXCollections.observableArrayList;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 /**
  * FXML Controller class
@@ -28,26 +31,34 @@ import javafx.scene.control.cell.PropertyValueFactory;
  * @author Bas
  */
 public class HenkController implements Initializable {
-    
-    
-    @FXML Button btnAdd;
-    @FXML TreeView tvTree;
-    @FXML TableView tvTable;
-    @FXML TextField txtNaam;
-    @FXML TextField txtPlaats;
-    @FXML TextField txtTelefoonnummer;
-    
+
+    @FXML
+    Button btnAdd;
+    @FXML
+    TreeView tvTree;
+    @FXML
+    TableView tvTable;
+    @FXML
+    TextField txtNaam;
+    @FXML
+    TextField txtPlaats;
+    @FXML
+    TextField txtTelefoonnummer;
+
     public ObservableList<Persoon> personen;
     private TableColumn naamCol;
     private TableColumn plaatsCol;
     private TableColumn telefoonCol;
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        tvTable.setEditable(true);
+        
         personen = observableArrayList();
         naamCol = new TableColumn("Naam");
         naamCol.setMinWidth(111.1);
@@ -55,18 +66,50 @@ public class HenkController implements Initializable {
         plaatsCol.setMinWidth(111.1);
         telefoonCol = new TableColumn("Telefoon");
         telefoonCol.setMinWidth(111.1);
+
+        naamCol.setCellValueFactory(new PropertyValueFactory<Persoon, String>("naam"));
+        plaatsCol.setCellValueFactory(new PropertyValueFactory<Persoon, String>("plaats"));
+        telefoonCol.setCellValueFactory(new PropertyValueFactory<Persoon, String>("telefoon"));
+
+        naamCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        naamCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Persoon, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Persoon, String> t) {
+                        ((Persoon) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())).setNaam(t.getNewValue());
+                    }
+                }
+        );
         
-        naamCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("naam"));
-        plaatsCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("plaats"));
-        telefoonCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("telefoon"));
+        plaatsCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        plaatsCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Persoon, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Persoon, String> t) {
+                        ((Persoon) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())).setPlaats(t.getNewValue());
+                    }
+                }
+        );
         
-        
+        telefoonCol.setCellFactory(TextFieldTableCell.forTableColumn());
+        telefoonCol.setOnEditCommit(
+                new EventHandler<CellEditEvent<Persoon, String>>() {
+                    @Override
+                    public void handle(CellEditEvent<Persoon, String> t) {
+                        ((Persoon) t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())).setTelefoon(t.getNewValue());
+                    }
+                }
+        );
+
         tvTable.setItems(personen);
-        tvTable.getColumns().addAll(naamCol,plaatsCol,telefoonCol);
-    }    
-    
-    public void btnAddClick(Event e)
-    {
+        tvTable.getColumns().addAll(naamCol, plaatsCol, telefoonCol);
+    }
+
+    public void btnAddClick(Event e) {
         personen.add(new Persoon(txtNaam.getText(), txtPlaats.getText(), txtTelefoonnummer.getText()));
+        System.out.println(personen.toString());
     }
 }
