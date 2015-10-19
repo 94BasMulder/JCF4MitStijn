b5ef541f257ac233ac33b09fe47e72e2e72f819d
Styntje <styntje@hotmail.nl>
2015-10-19 17:52:08 +0200

    shit gefixt, jullie weten hoe ik rollllll

--- HenkApp/src/henkapp/HenkController.java
+++ HenkApp/src/henkapp/HenkController.java
@@ -6,14 +6,21 @@
 package henkapp;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -29,11 +36,11 @@ public class HenkController implements Initializable {
     @FXML TextField txtNaam;
     @FXML TextField txtPlaats;
     @FXML TextField txtTelefoonnummer;
-
     
-    
-    
-    
+    public ObservableList<Persoon> personen;
+    private TableColumn naamCol;
+    private TableColumn plaatsCol;
+    private TableColumn telefoonCol;
     
     /**
      * Initializes the controller class.
@@ -41,11 +48,27 @@ public class HenkController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        personen = observableArrayList();
+        naamCol = new TableColumn("Naam");
+        naamCol.setMinWidth(tvTable.getWidth()/3);
+        plaatsCol = new TableColumn("Plaats");
+        plaatsCol.setMinWidth(tvTable.getWidth()/3);
+        telefoonCol = new TableColumn("Telefoon");
+        telefoonCol.setMinWidth(tvTable.getWidth()/3);
+        
+        tvTable.getColumns().addAll(naamCol,plaatsCol,telefoonCol);
+        naamCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("naam"));
+        plaatsCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("plaats"));
+        telefoonCol.setCellValueFactory(new PropertyValueFactory<Persoon,String>("telefoon"));
     }    
     
     public void btnAddClick(Event e)
     {
-    
+        personen.add(new Persoon(txtNaam.getText(), txtPlaats.getText(), txtTelefoonnummer.getText()));
+        update();
     }
     
+    private void update(){
+        
+    }
 }
---
+++ HenkApp/src/henkapp/Persoon.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package henkapp;
+
+/**
+ *
+ * @author Stijn
+ */
+public class Persoon {
+
+    private String naam;
+    private String plaats;
+    private String telefoon;
+
+    Persoon(String naam, String plaats, String telefoon) {
+        this.naam = naam;
+        this.plaats = plaats;
+        this.telefoon = telefoon;
+    }
+
+    @Override
+    public String toString() {
+        return naam + " - " + telefoon;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getPlaats() {
+        return plaats;
+    }
+
+    public void setPlaats(String plaats) {
+        this.plaats = plaats;
+    }
+
+    public String getTelefoon() {
+        return telefoon;
+    }
+
+    public void setTelefoon(String telefoon) {
+        this.telefoon = telefoon;
+    }
+}
