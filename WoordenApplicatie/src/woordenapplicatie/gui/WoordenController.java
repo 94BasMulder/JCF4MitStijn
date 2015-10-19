eee05849f9aa2ccc43ab99b4d451b9d3d8c10a5e
Styntje <styntje@hotmail.nl>
2015-10-19 16:02:51 +0200

    random
    shit is weer raar aan het doen

--- WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
+++ WoordenApplicatie/src/woordenapplicatie/gui/WoordenController.java
@@ -5,21 +5,17 @@ package woordenapplicatie.gui;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import static java.lang.System.in;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
-import java.util.function.Consumer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -73,33 +69,44 @@ public class WoordenController implements Initializable {
 
     @FXML
     private void aantalAction(ActionEvent event) {
+        taOutput.setText(Aantal(taInput.getText()));
+    }
+
+    public static String Aantal(String tekst) {
         Collection henk = new ArrayList<>();
-        String text = taInput.getText().toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
+        String text = tekst.toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
         for (String s : text.split(" ")) { //Constant ( N )
             henk.add(s.trim()); // weghalen onnodige spaties en toevoegen aan de lijst
         }
-        Set hashhenk = new HashSet<String>(henk); //omzetten naar HashSet om unieke resultaten te krijgen
+        Set hashhenk = new HashSet<>(henk); //omzetten naar HashSet om unieke resultaten te krijgen
         String out = "Totaal aantal woorden: " + henk.size() + "\n Aantal verschillende woorden: " + hashhenk.size();
-        taOutput.setText(out);
+        return out;
     }
 
     @FXML
     private void sorteerAction(ActionEvent event) {
+        taOutput.setText(Sorteer(taInput.getText()));
+    }
+
+    public static String Sorteer(String tekst) {
         Collection henk = new HashSet<>();
-        String text = taInput.getText().toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
+        String text = tekst.toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
         for (String s : text.split(" ")) { //Constant ( N )
             henk.add(s.trim()); // weghalen onnodige spaties en toevoegen aan de lijst
         }
-        Set hashhenk = new TreeSet<String>(Collections.reverseOrder()); //Aanmaken van een gesorteerde treeset die DESC sorteert
+        Set hashhenk = new TreeSet<>(Collections.reverseOrder()); //Aanmaken van een gesorteerde treeset die DESC sorteert
         hashhenk.addAll(henk);// Het toevoegen van alle items.
-        taOutput.setText(hashhenk + "\n");
+        return hashhenk + "";
     }
 
     @FXML
     private void frequentieAction(ActionEvent event) {
+        taOutput.setText(Frequentie(taInput.getText()));
+    }
+    public static String Frequentie(String tekst) {
         Map henkmap = new HashMap(); //Unieke waarden voor frquentie analyse
         Collection henklist = new ArrayList<>(); //Houd index en maakt het mogelijk om doorheen te lopen.
-        String text = taInput.getText().toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
+        String text = tekst.toLowerCase().replace("\r", " ").replace(",", "").replace("\n", " "); // Weghalen van comma's en enters
         for (String s : text.split(" ")) { //Constant ( N )
             if (henkmap.containsKey(s)) {
                 henkmap.replace(s, (int) henkmap.get(s) + 1); //Aanpassen van de waarde van een element
@@ -107,20 +114,24 @@ public class WoordenController implements Initializable {
                 henkmap.put(s, 1); //Ininteren van een element
             }
         }
-        
+
         String output = "";
-        henkmap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(entry -> henklist.add(entry.toString()+"\n")); //Sorteren map op value
-        taOutput.setText(henklist.toString());
+        henkmap.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(entry -> henklist.add(entry.toString() + "\n")); //Sorteren map op value
+        return henklist.toString();
     }
 
     @FXML
     private void concordatieAction(ActionEvent event) {
-        //Aanmaken van de map gekozen voor een treemap vanwege automatisch op volgorde zetten.
+        
+        taOutput.setText(Concordantie(taInput.getText()));
+    }
+    public static String Concordantie(String tekst) {
+         //Aanmaken van de map gekozen voor een treemap vanwege automatisch op volgorde zetten.
         Map henkmap = new TreeMap();
         //aanmaken van een int voor de referentie.
         int i = 1;
         //lijn ophalen om te kijken waar de woorden staan.
-        for (String line : taInput.getText().split("\n")) { //Liniear ( N^2 )
+        for (String line : tekst.split("\n")) { //Constant ( N )
             //woord voor woord gaan kijken of dat het bestaat in de treemap. zo niet voeg het toe aan de map, anders updaten van de value
             for (String word : line.split(" ")) { //Constant ( N )
                 word = word.replaceAll(",| ", "");
@@ -138,8 +149,6 @@ public class WoordenController implements Initializable {
         for (Object s : henkmap.keySet()) { //Constant ( N )
             output += s + ": " + henkmap.get(s) + "\n";
         }
-        taOutput.setText(output);
-
-    }
-
+        return output;
+     }
 }
